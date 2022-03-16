c99ebe3ed02d9031e8c8b33ef92b9e1426beada4
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-03-15 20:36:46 -0800

    added albums array

---
+++ array-four.js
@@ -0,0 +1,12 @@
+export const gamesList = [
+    { artist: 'Blondie',
+        album: 'American Gigolo',
+        tracks: ['Call Me', 'Night Drive', 'The Seduction', 'Love and Passion'],
+    
+    },
+    { artist: 'Depeche Mode',
+        album: 'Violator',
+        tracks: ['Personal Jesus', 'Enjoy the Silence', 'Policy of Truth', 'World in My Eyes'],
+    
+    }
+];
